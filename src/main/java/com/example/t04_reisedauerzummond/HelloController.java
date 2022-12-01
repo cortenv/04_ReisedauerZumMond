3eac1c42b8da10c71c3b5e7d32a6f7f8c4d72dff
cortenv <vera.corten@hotmail.com>
2022-12-01 16:33:30 +0100

    Initial commit

---
+++ src/main/java/com/example/t04_reisedauerzummond/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.t04_reisedauerzummond;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class HelloController {
+
+    @FXML
+    private RadioButton indays;
+
+    @FXML
+    private RadioButton inHours;
+
+    @FXML
+    private TextField TextFieldSpeed;
+
+    @FXML
+    private Button timeMath;
+
+    @FXML
+    private TextField textFieldDuration;
+}
---
+++ src/main/java/com/example/t04_reisedauerzummond/Mondreise.java
@@ -0,0 +1,22 @@
+package com.example.t04_reisedauerzummond;
+
+public class Mondreise {
+
+    Mondreise mondreise = new Mondreise(1500);
+
+    private double speedKmh;
+
+    public void Mondreise() {
+        double speed;
+    }
+
+    public void getTravelDurationDays() {
+        double speed;
+    }
+
+    public void getTravelDurationHours() {
+        double speed;
+    }
+
+
+}
