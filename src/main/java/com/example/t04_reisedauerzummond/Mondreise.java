78ff08b6f96ed727e6042eb5b40cae704d02e788
cortenv <vera.corten@hotmail.com>
2022-12-02 08:58:08 +0100

    first push

--- src/main/java/com/example/t04_reisedauerzummond/Mondreise.java
+++ src/main/java/com/example/t04_reisedauerzummond/Mondreise.java
@@ -2,8 +2,6 @@ package com.example.t04_reisedauerzummond;
 
 public class Mondreise {
 
-    Mondreise mondreise = new Mondreise(1500);
-
     private double speedKmh;
 
     public void Mondreise() {
